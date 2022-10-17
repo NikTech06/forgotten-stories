9f7230154bc0165f18ab011e3ad9788db64a6422
Nlk29 <78368286+NikTech06@users.noreply.github.com>
2022-10-17 18:13:16 +0200

    1813171022

--- Assets/Scripts/Player/CursorHandler.cs
+++ Assets/Scripts/Player/CursorHandler.cs
@@ -1,13 +1,39 @@
+// ##############################
+// #                            #
+// #  (C) 2022 by Niklas Köppl  #
+// #                            #
+// ##############################
+//
+// This software is licensed under Mozilla Public License 2.0
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Globalization;
+using System.Threading;
+using System.Linq;
+using Microsoft.Win32;
+using TMPro;
 
 public class CursorHandler : MonoBehaviour
 {
-    public bool hideOnStart = true;
+    public bool HideAtStart = true;
+
     void Start()
     {
-        
+        if(HideAtStart)
+        {
+            Hide();
+        }
     }
 
+    public void Hide()
+	{
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+
+    public void Show()
+	{
+        Cursor.lockState = CursorLockMode.Confined;
+	}
 }
